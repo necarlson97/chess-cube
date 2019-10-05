48d5ba0697999d42565f872e6802538e0ba2c785
necarlson97 <nilscarlson1997@gmail.com>
2019-10-04 22:39:21 -0400

    Add email launching to script

--- go.sh
+++ go.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
+set -e
+help_text="""
+Script for launching a python text-based chess ai.
+First argument changs the 'enviornment'. Options:
+  local - Run game with text and speech through terminal input
+  email - Run with input and output over email
+"""
+
 # Enter python virtual enviornment
 source env/bin/activate
-# Excecute
-python3.7 src/driver.py
+
+env=${1:-'local'}
+# Excecute depending on enviorment
+case $env in
+  local) python3.7 src/driver.py ;;
+  email) python3.7 src/email_chess.py ;;
+  *) echo -e "Unknwon env: '$env'.\n$help_text" ;;
+esac
